dccac54250d86ffccedc9a93465e9a17876db1ef
ForeverExiled <foreverexiled23@gmail.com>
2024-06-16 14:23:05 +0500

    core database connection class

--- core/database/CJ_Connection.php
+++ core/database/CJ_Connection.php
@@ -1,7 +1,21 @@
 <?php
 class CJ_Connection
 {
-    function get_connection(): string {
-        return 'connection';
+    function __construct() {
+        require 'config/database.php';
+        $this->db_params = $db_params;
+    }
+
+    function get_connection() {
+        $connection = new mysqli(
+            $this->db_params['hostname'],
+            $this->db_params['username'],
+            $this->db_params['password'],
+            $this->db_params['database']
+        );
+        if ($connection->connect_error) {
+            die ("Connection failed:".$connection->connect_error);
+        }
+        return $connection;
     }
 }
